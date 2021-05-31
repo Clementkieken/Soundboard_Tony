9f5cb4d65cb449b9d6806801a9bf5d369effae2e
Clement Kieken <votreemail>
2021-05-31 18:48:26 +0200

    all sound js

--- Soundboard_Tony/js/app.js
+++ Soundboard_Tony/js/app.js
@@ -1,3 +1,5 @@
+// Initializing 
+
 // Music pour Jaws
 var jawsButton = document.getElementById('jawsThumbnail');
 
@@ -19,3 +21,43 @@ function playElevator() {
 elevatorButton.addEventListener('click', playElevator);
 
 console.log()
+
+// Music pour Qui veut Gagner des Millions
+var millionsButton = document.getElementById('millionsThumbnail');
+
+function playMillions() {
+    let audio = new Audio('../ressources/millions_theme.mp3'); // '../ressources/' + variable;
+    audio.play();
+  }
+
+millionsButton.addEventListener('click', playMillions);
+
+// Music pour Baby Shark
+var babySharkButton = document.getElementById('babySharkThumbnail');
+
+function playBabyShark() {
+    let audio = new Audio('../ressources/BabyShark_cut_theme.mp3'); // '../ressources/' + variable;
+    audio.play();
+  }
+
+babySharkButton.addEventListener('click', playBabyShark);
+
+// Music pour Dun Dun Duuuuun
+var dunButton = document.getElementById('dunThumbnail');
+
+function playDun() {
+    let audio = new Audio('../ressources/dun.mp3'); // '../ressources/' + variable;
+    audio.play();
+  }
+
+dunButton.addEventListener('click', playDun);
+
+// Music pour badum
+var badumButton = document.getElementById('badumThumbnail');
+
+function playBadum() {
+    let audio = new Audio('../ressources/badum.mp3'); // '../ressources/' + variable;
+    audio.play();
+  }
+
+badumButton.addEventListener('click', playBadum);
